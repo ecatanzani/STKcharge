a7f43aa6fb56b5f90fdd26af3d3a3f0701855ad9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-07-01 09:29:55 +0200

    Updated including outputDir as inpout flag

--- source/main.cpp
+++ source/main.cpp
@@ -13,12 +13,14 @@ int main(int argc, char **argv)
     opt.addUsage(" -h  --help                                                   Prints this help");
     opt.addUsage(" -i  --input          <path_to_input_DATA_list>           (*) Input DATA list");
     opt.addUsage(" -o  --output         <path_to_output_TFile>                  Output ROOT TFile");
+    opt.addUsage(" -d  --outputDir      <path_to_output_TFile_dir>              Output ROOT TFile directory");
     opt.addUsage(" -v  --verbose                                                Verbose output");
     opt.addUsage("");
 
     opt.setFlag("help", 'h');
     opt.setOption("input", 'i');
     opt.setOption("output", 'o');
+    opt.setOption("outputDir", 'd');
     opt.setFlag("verbose", 'v');
 
     opt.processCommandArgs(argc, argv);
@@ -46,6 +48,8 @@ int main(int argc, char **argv)
         inputPath = opt.getValue('i');
     if (opt.getValue("output") || opt.getValue('o'))
         outputPath = opt.getValue('o');
+    if (opt.getValue("outputDir") || opt.getValue('d'))
+        outputPath = opt.getValue('d');
     if (opt.getFlag("verbose") || opt.getFlag('v'))
         verbose = opt.getFlag('v');
     
